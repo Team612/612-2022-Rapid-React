7c847c14e0af0e4098f84ed3fd62c52fc9ded5d1
nav1211 <67112220+nav1211@users.noreply.github.com>
2022-01-24 20:43:52 -0500

    Add files via upload

--- src/main/java/frc/robot/subsystems/Climb.java
+++ src/main/java/frc/robot/subsystems/Climb.java
@@ -1,21 +1,29 @@
 package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import frc.robot.Constants;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.motorcontrol.Talon;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;    
 public class Climb extends SubsystemBase {
     //defines both Talons and Solenoids
     private WPI_TalonSRX pivotMotor = new WPI_TalonSRX(Constants.Talon);
     private final DoubleSolenoid secondClimb = new DoubleSolenoid(PneumaticsModuleType.REVPH, Constants.secondForward, Constants.secondReverse);
     private final DoubleSolenoid climb = new DoubleSolenoid(PneumaticsModuleType.REVPH, Constants.forward, Constants.reverse);
-
+    //private final TalonFX pivotMotor2 = new TalonFX(0, "roboRio");
     //Pushes the piston out
     public void extendArm(){
         climb.set(Value.kForward);
-        secondClimb.set(Value.kForward);
+        //secondClimb.set(Value.kForward);
 
+    }
+
+    //partial extention
+    public void partialExtention(){
+        climb.set(Value.kForward);
+        secondClimb.set(Value.kReverse);
     }
     
     //Brings the piston in
