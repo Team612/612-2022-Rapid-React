a08241080e0d4e6a4b8d08ff796c3d70668968e7
ctrl-alt-delete101 <57509291+ctrl-alt-delete101@users.noreply.github.com>
2022-01-29 23:14:31 -0500

    included potential code to find velocity over time from acceleration

--- src/main/java/frc/robot/subsystems/IntegralOdometry.java
+++ src/main/java/frc/robot/subsystems/IntegralOdometry.java
@@ -4,6 +4,60 @@
 
 package frc.robot.subsystems;
 /** Add your docs here. */
-public class IntegralOdometry {   
-   
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Twist2d;
+import edu.wpi.first.util.WPIUtilJNI;
+import edu.wpi.first.wpilibj.BuiltInAccelerometer;
+
+public class IntegralOdometry {
+    private BuiltInAccelerometer m_accelerometer;
+    private Pose2d m_currentPosition;
+    private double m_previousTime = -1;
+
+    private Rotation2d m_gyroOffset;
+    private Rotation2d m_previousAngle;
+
+    public IntegralOdometry(BuiltInAccelerometer accelerometer, Rotation2d gyroAngle, Pose2d initialPosition){
+        m_accelerometer = accelerometer;
+        m_currentPosition = initialPosition;
+        m_gyroOffset = m_currentPosition.getRotation().minus(gyroAngle);
+        m_previousAngle = initialPosition.getRotation();
+    }
+
+    public void resetPosition(Pose2d currentPosition, Rotation2d gyroAngle){
+        m_currentPosition = currentPosition;
+        m_previousAngle = currentPosition.getRotation();
+        m_gyroOffset = currentPosition.getRotation().minus(gyroAngle);
+    }
+
+    public Pose2d getPoseMeters(){
+        return m_currentPosition;
+    }
+
+    public Pose2d getVelocity(double currentTime, Rotation2d currentAngle, double getAccelX, double getAccelY, double getAccelZ){
+        double delta_t = m_previousTime >= 0? currentTime - m_previousTime : 0.0;
+        m_previousTime = currentTime;
+
+        var angle = currentAngle.plus(m_gyroOffset);
+        Pose2d accel_vector = new Pose2d(getAccelX, getAccelY, new Rotation2d(getAccelZ));
+
+        var newPose = 
+            m_currentPosition.exp(
+                new Twist2d(
+                    accel_vector.getX() * delta_t,
+                    accel_vector.getY() * delta_t,
+                    currentAngle.minus(m_previousAngle).getRadians()
+                )
+            );
+        m_previousAngle = currentAngle;
+        m_currentPosition = new Pose2d(newPose.getTranslation(), currentAngle);
+
+        return newPose;
+    }
+
+    public Pose2d getVelocityWithTime(Rotation2d gyroAngle, double getAccelX, double getAccelY, double getAccelZ){
+        return getVelocity(WPIUtilJNI.now() * 1.0e-6, gyroAngle, getAccelX, getAccelY, getAccelZ);
+    }
 }
