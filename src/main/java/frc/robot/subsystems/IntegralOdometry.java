054e7d93ed819825056e33361f9fe2fd98867878
Arjun Mudda <57509291+ctrl-alt-delete101@users.noreply.github.com>
2022-01-31 22:39:01 -0500

    included potential code to find velocity over time from acceleration (#17)
    * included potential code to find velocity over time from acceleration
    
    * Acceleration to Chassis Velocity works

--- src/main/java/frc/robot/subsystems/IntegralOdometry.java
+++ src/main/java/frc/robot/subsystems/IntegralOdometry.java
@@ -4,6 +4,80 @@
 
 package frc.robot.subsystems;
 /** Add your docs here. */
-public class IntegralOdometry {   
-   
+
+import javax.lang.model.util.ElementScanner6;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.geometry.Twist2d;
+import edu.wpi.first.util.WPIUtilJNI;
+import edu.wpi.first.wpilibj.BuiltInAccelerometer;
+
+public class IntegralOdometry{
+    private BuiltInAccelerometer m_accelerometer;
+    private Pose2d m_currentPosition;
+    private double m_previousTime = -1;
+
+    private Rotation2d m_gyroOffset;
+    private Rotation2d m_previousAngle;
+
+    private double accumulated_velocity;
+
+    public IntegralOdometry(Rotation2d gyroAngle, Pose2d initialPosition){
+        m_currentPosition = initialPosition;
+        m_gyroOffset = m_currentPosition.getRotation().minus(gyroAngle);
+        m_previousAngle = initialPosition.getRotation();
+    }
+
+    public void resetPosition(Pose2d currentPosition, Rotation2d gyroAngle){
+        m_currentPosition = currentPosition;
+        m_previousAngle = currentPosition.getRotation();
+        m_gyroOffset = currentPosition.getRotation().minus(gyroAngle);
+    }
+
+    public Pose2d getPoseMeters(){
+        return m_currentPosition;
+    }
+
+    public double getVelocity(double currentTime, double getAccelX){
+        double dt = m_previousTime >= 0? currentTime - m_previousTime : 0.0;
+        m_previousTime = currentTime;
+        double threshold = 0.02;
+        boolean ismoving = getAccelX > threshold ? true : false;
+        if (ismoving) {
+            accumulated_velocity += (getAccelX * 9.81)*dt;
+        }
+        else {
+            accumulated_velocity = 0;
+        }
+
+        getAccelX  = getAccelX >= 0.02 ? getAccelX : 0.0;
+        return accumulated_velocity;
+    }
+
+    public Pose2d getPosition(double currentTime, Rotation2d currentAngle, double getVelX){
+        double delta_t = m_previousTime >= 0? currentTime - m_previousTime : 0.0;
+        m_previousTime = currentTime;
+
+        var angle = currentAngle.plus(m_gyroOffset);
+        Translation2d accel_vector = new Translation2d(getVelX, 0);
+        var newPose = 
+            m_currentPosition.exp(
+                new Twist2d(
+                    accel_vector.getX() * delta_t,
+                    0, //must set to 0 for non holonomic drivetrains
+                    angle.minus(m_previousAngle).getRadians()
+                )
+            );
+        m_previousAngle = angle;
+
+        m_currentPosition = new Pose2d(newPose.getTranslation(), angle);
+
+        return m_currentPosition;
+    }
+
+    public Pose2d getPositionWithTime(Rotation2d gyroAngle, double getVelX){
+        return getPosition(WPIUtilJNI.now() * 1.0e-6, gyroAngle, getVelX);
+    }
 }
--- src/main/java/frc/robot/subsystems/RomiDrivetrain.java
+++ src/main/java/frc/robot/subsystems/RomiDrivetrain.java
@@ -3,12 +3,15 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.subsystems;
-
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.util.WPIUtilJNI;
 import edu.wpi.first.wpilibj.BuiltInAccelerometer;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.sensors.RomiGyro;
 
 public class RomiDrivetrain extends SubsystemBase {
   private static final double kCountsPerRevolution = 1440.0;
@@ -21,10 +24,12 @@ public class RomiDrivetrain extends SubsystemBase {
   private final Encoder m_rightEncoder = new Encoder(6, 7);
 
   private final BuiltInAccelerometer accelerometer = new BuiltInAccelerometer();
+  private final RomiGyro m_gyro;
+  private final IntegralOdometry m_integral;
 
   // Set up the differential drive controller
   private final DifferentialDrive m_diffDrive = new DifferentialDrive(m_leftMotor, m_rightMotor);
-
+  private final DifferentialDriveOdometry m_odometry;
 
   /** Creates a new RomiDrivetrain. */
   public RomiDrivetrain() {
@@ -32,9 +37,13 @@ public class RomiDrivetrain extends SubsystemBase {
     m_leftEncoder.setDistancePerPulse((Math.PI * kWheelDiameterInch) / kCountsPerRevolution);
     m_rightEncoder.setDistancePerPulse((Math.PI * kWheelDiameterInch) / kCountsPerRevolution);
     resetEncoders();
-
+    m_gyro  = new RomiGyro();
+    m_integral = new IntegralOdometry(m_gyro.getRotation2d(), new Pose2d());
     // Invert right side since motor is flipped
     m_rightMotor.setInverted(true);
+    m_odometry = new DifferentialDriveOdometry(m_gyro.getRotation2d());
+
+
   }
 
 
@@ -66,11 +75,17 @@ public class RomiDrivetrain extends SubsystemBase {
 
   public double getAccelZ(){
     return accelerometer.getZ();
+
   }
 
   @Override
   public void periodic() {
+    //m_odometry.update(m_gyro.getRotation2d(), m_leftEncoder.getDistance(), m_rightEncoder.getDistance());
     // This method will be called once per scheduler run
+
+    double velocity = Math.floor(m_integral.getVelocity(WPIUtilJNI.now() * 1.0e-6, getAccelX()) * 1000) / 1000;
+
+    System.out.println("Velocity: " + velocity);
   }
 
   @Override
