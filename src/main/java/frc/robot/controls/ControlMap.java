faeeec4b5416a212eb298fb423ca9a1a053cabc5
1756166 <45674867+1756166@users.noreply.github.com>
2022-03-01 16:20:45 -0500

    updated controlmap

--- src/main/java/frc/robot/controls/ControlMap.java
+++ src/main/java/frc/robot/controls/ControlMap.java
@@ -1,25 +1,4 @@
 package frc.robot.controls;
-
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-
-
-/** Add your docs here. */
-public class ControlMap {
-    //Driver and Gunner Defined
-public static XboxController driver = new XboxController(1);
-public static XboxController gunner = new XboxController(0);
-
- 
-
-public static final JoystickButton groundToMid = new JoystickButton(gunner, 0);
-public static final JoystickButton midToHigh = new JoystickButton(gunner, 1);
-public static final JoystickButton highToTraversal = new JoystickButton(gunner, 2);
-
-package frc.robot.controls;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
