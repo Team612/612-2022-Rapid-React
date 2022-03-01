3d3e3087de70fe2817a092619b8a770b39595db5
1756166 <anoopdavid@outlook.com>
2022-03-01 18:08:25 -0500

    added intake togglability functionality

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -4,16 +4,17 @@
 
 package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Intake extends SubsystemBase {
   /** Creates a new Intake. */
   /*private final Servo topLeft = new Servo(1);
-  private final Servo topRight = new Servo(1);
-  private final Servo bottomLeft = new Servo(1);
-  private final Servo bottomRight = new Servo(1); 
-  private final Servo wrist = new Servo(2);*/
+  private final Servo topRight = new Servo(1);*/
+  private final Servo bottomLeft = new Servo(Constants.left_intake_servo);
+  private final Servo bottomRight = new Servo(Constants.right_intake_servo);
 
 
   private final int TOP_POSITION = 0;
@@ -58,26 +59,20 @@ public class Intake extends SubsystemBase {
     // }
   }
 
-  /*public void TopServoOpen() {
-    //opens the top claw
-    topLeft.setAngle(90);
-    topRight.setAngle(-90);
-  }
+  
   public void BottomServoOpen() {
     //opens the bottom claw
     bottomLeft.setAngle(90);
-    bottomRight.setAngle(-90);
+    bottomRight.setAngle(90);
+    System.out.println("opengrabber");
   }
-  public void TopServoClose() {
-    //closes the top claw
-    topLeft.setAngle(270);
-    topRight.setAngle(-270);
-  }
+  
   public void BottomServoClose() {
     //closes the bottom claw
-    bottomLeft.setAngle(270);
-    bottomRight.setAngle(-270);
-  }*/
+    bottomLeft.setAngle(180);
+    bottomRight.setAngle(0);
+    System.out.println("closegrabber");
+  }
   
   /*public void WristOpen() {
     //dynamically moves the wrist for the second claw
