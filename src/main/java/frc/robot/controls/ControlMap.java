b97b9478e30df2373b6c2e01d4e2ed0fa16fe9f9
nav1211 <67112220+nav1211@users.noreply.github.com>
2022-03-09 17:00:31 -0500

    Update ControlMap.java

--- src/main/java/frc/robot/controls/ControlMap.java
+++ src/main/java/frc/robot/controls/ControlMap.java
@@ -1,44 +1,48 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot.controls;
+
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
-public class ControlMap{
-    //driver ports
-    private static int DRIVER_PORT = 0;
-    private static int GUNNER_PORT = 1;
-    
-    //Controller objects
-    public static Joystick driver = new Joystick(DRIVER_PORT);
-    public static Joystick gunner = new Joystick(GUNNER_PORT);
-   
-    public static JoystickButton GUNNER_A = new JoystickButton(gunner, 1); //A
-    public static JoystickButton GUNNER_B = new JoystickButton(gunner, 2); //B
-    public static JoystickButton GUNNER_X = new JoystickButton(gunner, 3); //X
-    public static JoystickButton GUNNER_Y = new JoystickButton(gunner, 4); //Y
-    public static JoystickButton GUNNER_LB = new JoystickButton(gunner, 5); //LB
-    public static JoystickButton GUNNER_RB = new JoystickButton(gunner, 6); //RB
-    public static JoystickButton GUNNER_BACK = new JoystickButton(gunner, 7); 
-    public static JoystickButton GUNNER_START = new JoystickButton(gunner, 8); 
-    public static JoystickButton GUNNER_LJ_BUTTON = new JoystickButton(gunner, 9); //
-    public static JoystickButton GUNNER_RJ_BUTTON = new JoystickButton(gunner, 10); //
-    
-    
-    // public static JoystickButton climbExtend = new JoystickButton(gunner, 1); //A
-    // public static JoystickButton climbRetract = new JoystickButton(gunner, 2); //B
-    // public static JoystickButton staticHookOn = new JoystickButton(gunner, 3); //X
-    // public static JoystickButton staticHookOff = new JoystickButton(gunner, 4); //Y
-    // public static JoystickButton arm_down = new JoystickButton(gunner, 5); //LB
-    // public static JoystickButton arm_up = new JoystickButton(gunner, 6); //RB
+/** Add your docs here. */
+public class ControlMap {
+    //Initialize keyboard
+    public static Joystick m_control = new Joystick(0);
 
+    //button bindings for the left solenoid
+    public static JoystickButton m_z = new JoystickButton(m_control, 1);
+    public static JoystickButton m_x = new JoystickButton(m_control, 2);
+    public static JoystickButton m_c = new JoystickButton(m_control, 3);
+    public static JoystickButton m_v = new JoystickButton(m_control, 4);
 
+    //button bindings for the left servo intake claw
+    public static JoystickButton m_b = new JoystickButton(m_control, 5);
+    public static JoystickButton m_n = new JoystickButton(m_control, 6);
 
-    /*public static JoystickButton topOpen = new JoystickButton(gunner, 10);
-    public static JoystickButton topClose = new JoystickButton(gunner, 11);
-    /*public static JoystickButton topToggle = new JoystickButton(gunner, 69);
-    public static JoystickButton bottomToggle = new JoystickButton(gunner, 420);
-    public static JoystickButton rotateWristUp = new JoystickButton(gunner, 10);
-    public static JoystickButton rotateWristDown = new JoystickButton(gunner, 11);
-    public static JoystickButton wristToggle = new JoystickButton(gunner, 42);*/
+    //button bindings for the climb talon pivot
+    public static JoystickButton m_m = new JoystickButton(m_control, 7);
 
+    //button bindings for the left static hook servo
+    public static JoystickButton m_l = new JoystickButton(m_control, 8);
+    public static JoystickButton m_k = new JoystickButton(m_control, 9);
+
+    //button bindings for the right solenoid
+    public static JoystickButton m_j = new JoystickButton(m_control, 10);
+    public static JoystickButton m_h = new JoystickButton(m_control, 11);
+    public static JoystickButton m_g = new JoystickButton(m_control, 12);
+    public static JoystickButton m_f = new JoystickButton(m_control, 13);
     
+    //button bindings for the right static hook servo
+    public static JoystickButton m_q = new JoystickButton(m_control, 14);
+    public static JoystickButton m_t = new JoystickButton(m_control, 15);
+
+    //button bindings for the right servo intake claw
+    public static JoystickButton m_y = new JoystickButton(m_control, 16);
+    public static JoystickButton m_u = new JoystickButton(m_control, 17);
+
+    //button bindings for the intake arm talon
+    public static JoystickButton m_i = new JoystickButton(m_control, 18);
 }
