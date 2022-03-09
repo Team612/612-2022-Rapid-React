0b6168b7ad4ced11735b9843e11bc8583f943adb
Colin M Stables <39416636+ColinMStables@users.noreply.github.com>
2022-03-09 17:57:47 -0500

    Create Logging.java
    Created logging file

---
+++ src/main/java/frc/robot/commands/Logging.java
@@ -0,0 +1,43 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Drivetrain;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Logging extends CommandBase {
+  /** Creates a new Logging. */
+  public Drivetrain m_drivetrain;
+  public Logging(Drivetrain drivetrain) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    m_drivetrain = drivetrain;
+    addRequirements(m_drivetrain);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    JSONObject Time = new JSONObject();
+    JSONObject NoTime = new JSONObject();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
