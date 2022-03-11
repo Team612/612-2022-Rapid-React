79238db75d9b2c922927527643dd9492bae7362d
Colin M Stables <39416636+ColinMStables@users.noreply.github.com>
2022-03-10 20:07:45 -0500

    Updated logging to give it functionality
    Added small logging functionality. This will create a very long string of data when the methods are used. Working on making the data tree more readable. (Visual studio isn't showing me errors in the code so sorry for any errors). There may be some issues with some data trying to be written at the same time

--- src/main/java/frc/robot/commands/Logging.java
+++ src/main/java/frc/robot/commands/Logging.java
@@ -10,6 +10,8 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import java.io.FileWriter;
 import java.io.IOException;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class Logging extends CommandBase {
   /** Creates a new Logging. */
@@ -23,13 +25,85 @@ public class Logging extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    //Creates the main structure of the json file
+    JSONObject seconds = new JSONObject();
+    seconds.put("Encoder","running");
+    seconds.put("Arm","running");
+
     JSONObject Time = new JSONObject();
+    Time.put("TimeSensititve",seconds);
+
+    JSONObject encoderValues = new JSONObject();
+    encoderValues.put("Time","Values");
+    
+    JSONArray encoder = new JSONArray();
+    encoder.add("Encoder", encoderValues);
+
     JSONObject NoTime = new JSONObject();
+    NoTime.put("NonTimeSensitive",encoder);
+
+    JSONArray fulltime = new JSONArray();
+    fulltime.add(Time);
+    fulltime.add(NoTime);
+
+    ObjectMapper mapper = new ObjectMapper();
+
+    try {
+        FileWriter info = new FileWriter("infotest.json");
+        info.write(json);
+        info.close();
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+    try {
+    FileWriter info1 = new FileWriter("info.json");
+    JSONArray startinfo = new JSONArray();
+    info1.write(startinfo);
+    info1.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+  }
+
+ 
+  public void logNumericalData(String device, float data, float time){
+    //try to read and append the data gathered
+    JSONParser jsonParser = new JSONParser();
+    try{
+      Object obj = jsonParser.parse(new FileReader("info.json"));
+      JSONArray jsonArray = (JSONArray)obj;
+      JSONObject info = new JSONObject();
+      info.put(device, time + " - " + data);
+      FileWriter file = new FileWriter("info.json");
+      file.write(jsonArray.toJSONString());
+      file.flush();
+      file.close();
+    } catch (ParseException | IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void logBoolData(String device, boolean data, float time){
+    //try to read and append the data gathered
+    JSONParser jsonParser = new JSONParser();
+    try{
+      Object obj = jsonParser.parse(new FileReader("info.json"));
+      JSONArray jsonArray = (JSONArray)obj;
+      JSONObject info = new JSONObject();
+      info.put(device, time + " - " + data);
+      FileWriter file = new FileWriter("info.json");
+      file.write(jsonArray.toJSONString());
+      file.flush();
+      file.close();
+    } catch (ParseException | IOException e) {
+      e.printStackTrace();
+    }
+  }
 
   // Called once the command ends or is interrupted.
   @Override
