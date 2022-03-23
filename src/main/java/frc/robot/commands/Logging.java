29090b71c9283d0395332507ebadc3e54d0c2292
Colin M Stables <39416636+ColinMStables@users.noreply.github.com>
2022-03-23 15:59:06 -0400

    Added functionality to Logging.java
    Hopefully fixed the issues from last time.

--- src/main/java/frc/robot/commands/Logging.java
+++ src/main/java/frc/robot/commands/Logging.java
@@ -4,14 +4,21 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.subsystems.Drivetrain;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Drivetrain;
+
 
 public class Logging extends CommandBase {
   /** Creates a new Logging. */
@@ -25,43 +32,18 @@ public class Logging extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    //Creates the main structure of the json file
-    JSONObject seconds = new JSONObject();
-    seconds.put("Encoder","running");
-    seconds.put("Arm","running");
-
-    JSONObject Time = new JSONObject();
-    Time.put("TimeSensititve",seconds);
-
-    JSONObject encoderValues = new JSONObject();
-    encoderValues.put("Time","Values");
-    
-    JSONArray encoder = new JSONArray();
-    encoder.add("Encoder", encoderValues);
-
-    JSONObject NoTime = new JSONObject();
-    NoTime.put("NonTimeSensitive",encoder);
-
+    //Creates a blank json file to write on.
     JSONArray fulltime = new JSONArray();
-    fulltime.add(Time);
-    fulltime.add(NoTime);
 
     ObjectMapper mapper = new ObjectMapper();
 
     try {
-        FileWriter info = new FileWriter("infotest.json");
+        String json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(fulltime);
+        FileWriter info = new FileWriter("info.json");
         info.write(json);
         info.close();
     } catch (IOException e) {
         e.printStackTrace();
-    }
-    try {
-    FileWriter info1 = new FileWriter("info.json");
-    JSONArray startinfo = new JSONArray();
-    info1.write(startinfo);
-    info1.close();
-    } catch (IOException e) {
-      e.printStackTrace();
     }
   }
 
@@ -70,47 +52,87 @@ public class Logging extends CommandBase {
   public void execute() {
   }
 
- 
+ //logs the name of the device, the time and what data you would like to store in a float type.
   public void logNumericalData(String device, float data){
-    //try to read and append the data gathered
     JSONParser jsonParser = new JSONParser();
-    Runtime time = Runtime.getRuntime();
-    try{
-      Object obj = jsonParser.parse(new FileReader("info.json"));
-      JSONArray jsonArray = (JSONArray)obj;
-      JSONObject info = new JSONObject();
-      info.put(device, time + " - " + data);
-      FileWriter file = new FileWriter("info.json");
-      file.write(jsonArray.toJSONString());
-      file.flush();
-      file.close();
-    } catch (ParseException | IOException e) {
-      e.printStackTrace();
+		try {
+      //grab the runtime.
+      Runtime rt = Runtime.getRuntime();
+      Object obj;
+      //parse the already created json file.
+			obj = jsonParser.parse(new FileReader("info.json"));
+            //Creates the array to be appended.
+            JSONArray jsonArray = (JSONArray) obj;            
+            //System.out.println(jsonArray);
+            //Creates all the log data into one string.
+            String jsonData = "Time: " + rt + " Data: " + data;
+            HashMap<String,Object> jsonHash = new HashMap<String,Object>();
+            //Adds the data to a hashmap for better visualization.
+            jsonHash.put(device,jsonData);
+            //creates a json object and fills it with the data in the hashmap, then that data is added to the main array in the json file.
+            JSONObject info = new JSONObject(jsonHash);
+            jsonArray.add(info);
+
+            try{
+              //writes into the file and appends the data.
+                FileWriter file = new FileWriter("info.json");
+                    file.write(jsonArray.toJSONString());
+                    file.flush();
+                    file.close();
+                } catch(IOException e){
+                    e.printStackTrace();
+                }
+            }
+
+            //all the exceptions required.
+      catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		} catch (org.json.simple.parser.ParseException e1) {
+      e1.printStackTrace();
     }
   }
-
-  //device should be the name of the data your grabbing, data is what you want to log and time is the current time
+  //Logs the device name, time and the data in a bool type. Same code as above.
   public void logBoolData(String device, boolean data){
-    //try to read and append the data gathered
     JSONParser jsonParser = new JSONParser();
-    Runtime time = Runtime.getRuntime();
-    try{
-      Object obj = jsonParser.parse(new FileReader("info.json"));
-      JSONArray jsonArray = (JSONArray)obj;
-      JSONObject info = new JSONObject();
-      info.put(device, time + " - " + data);
-      FileWriter file = new FileWriter("info.json");
-      file.write(jsonArray.toJSONString());
-      file.flush();
-      file.close();
-    } catch (ParseException | IOException e) {
-      e.printStackTrace();
+		try {
+      Runtime rt = Runtime.getRuntime();
+      Object obj;
+			obj = jsonParser.parse(new FileReader("info.json"));
+            JSONArray jsonArray = (JSONArray) obj;            
+            //System.out.println(jsonArray);
+
+            String jsonData = "Time: " + rt + " Data: " + data;
+            HashMap<String,Object> jsonHash = new HashMap<String,Object>();
+            jsonHash.put(device,jsonData);
+            JSONObject info = new JSONObject(jsonHash);
+            jsonArray.add(info);
+
+            try{
+                FileWriter file = new FileWriter("info.json");
+                    file.write(jsonArray.toJSONString());
+                    file.flush();
+                    file.close();
+                } catch(IOException e){
+                    e.printStackTrace();
+                }
+            }
+
+      catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		} catch (org.json.simple.parser.ParseException e1) {
+      e1.printStackTrace();
     }
   }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+
+  }
 
   // Returns true when the command should end.
   @Override
